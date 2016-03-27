8f0adb3da465c7d25d251043b6048728585b9416
Emanuel Czirai <res.pe.cing+commits@gmail.com>
2016-03-27 01:05:06 +0100

    make log calls easier to type
    also handle impossible case, ensure request is cancelled!

--- background.js
+++ background.js
@@ -27,10 +27,19 @@ function log(level, msg) {//should never pass NONE level when calling!
   if (level <= loglevel) {
     if (level == loglevels.WARN) {
       console.warn(msg);
+      //TODO: add console.err  or is it .error ? check and see!
     }else{
       console.log(msg);
     }
   }
+}
+
+function logverb(msg) {
+  log(loglevels.VERBOSE, msg);
+}
+
+function logwarn(msg) {
+  log(loglevels.WARN, msg);
 }
 
 //NB: I left these two in 'ignore' as samples, but since I'm planning on permanently using HTTPS-Everywhere with 'Block HTTP requests', I won't be doing any HTTP requests ever, and thus I can afford to also set 'includeSubDomains' below.
@@ -54,9 +63,9 @@ forceDisable = forceDisable.concat([ //src: http://www.w3schools.com/jsref/jsref
 //Note: access the following console.log() messages by going chrome://extensions/ then clicking the 'background page' seen as 'Inspect views: background page' under this extension (HSTS Everywhere) then chosing the 'Console' tab.
 
 var cwr = chrome.webRequest;
-//TODO: make logging optional! eg. depending on a var that can be set on Console!
 
 //so, this part will force all http attempts to go through https ! and if they can't because no https server is listening well, too bad... you'll get a ERR_CONNECTION_REFUSED as usual.
+//why did I need this? well, the links from Variable bindings from here, for example: https://github.com/carols10cents/rustlings#variable-bindings were all http and https-everywhere wasn't doing anything about them (whilst 'Block All HTPP requests' being set was stropping anything from loading - so, don't need https-everywhere anymore now, i suppose)
 cwr.onBeforeRequest.addListener(
     function(details) {
 //  console.log(details);
@@ -65,14 +74,12 @@ cwr.onBeforeRequest.addListener(
     if (redirecturl.substring(0,5) === "http:") {
       ishttp=true;
       details.url = redirecturl = redirecturl.slice(0,4) + "s" + redirecturl.slice(4);
-      log(loglevels.VERB, "ForcedHTTPS as: '"+redirecturl+"'")
+      logverb("ForcedHTTPS as: '"+redirecturl+"'");
     } else {
-      log(loglevels.WARN, "!!!Sneaked url: "+redirecturl);//XXX: if this happens, it's some serious issue afoot!
-    }
-/*    if (details.url.substring(0,5) === "http:") {//XXX: changing details.url doesn't matter!
-      console.log("IMPOSSIBLE! "+details.url);
+      logwarn("!!!Sneaked url: "+redirecturl);//XXX: if this happens, it's some serious issue afoot!
+      alert("!!! IMPOSSIBIRU! Congratulations, you've just discovered the impossible! This url '"+redirecturl+"' was not a http url but was caught by the http filter. The request is about to be stopped after you exit this, but make a screenshot first and show it around, since this probably means there's a bug in chromium!");
       return { cancel: true }
-    }*/
+    }
     return { redirectUrl: redirecturl } //XXX: but HTTPS-Everywhere with 'Block HTTP request' will still cancel it! because details.url it receives is the same http one!
 }, {
   urls: ["http://*/*"],
@@ -88,12 +95,12 @@ cwr.onHeadersReceived.addListener(
     force_disable=0
     if ( forceDisable.indexOf(hostn) > -1 ) {
       force_disable=1
-      log(loglevels.VERB, "Host in forceDisable list: "+fullh); //eg. https://pastebin.com
+      logverb("Host in forceDisable list: "+fullh); //eg. https://pastebin.com
     }
 
 //    ignored=false;
 		if ( ignore.indexOf(hostn) > -1 ) {
-      log(loglevels.VERB, "Host in ignore list: "+fullh); //eg. https://pastebin.com
+      logverb("Host in ignore list: "+fullh); //eg. https://pastebin.com
 //      ignored=true;
 			if (! force_disable) {//forceDisable list takes precedence over ignore list!
         return { }; //TODO: Does {} mean that no changes were perfomed to headers? so original headers are preserved? check online docs to see if this is so!
@@ -108,7 +115,7 @@ cwr.onHeadersReceived.addListener(
           //remove HSTS header if host is in ignored list; OR we can leave it and set max-age to 0 to disable HSTS...
           details.responseHeaders.splice(i,1); //remove 1 item from index i; src: http://www.w3schools.com/jsref/jsref_splice.asp
         } else {*/
-        log(loglevels.VERB, "HSTS already set in host's response headers: "+fullh+" "+details.responseHeaders[i].value); //eg. grc.com  max-age=31536000; preload
+        logverb("HSTS already set in host's response headers: "+fullh+" "+details.responseHeaders[i].value); //eg. grc.com  max-age=31536000; preload
         if ( ! force_disable ) {
     			return { };
         }
@@ -128,7 +135,7 @@ cwr.onHeadersReceived.addListener(
         //also note, if 'broken HTTPS' (eg. https://rustbyexample.com since it's hosted by github) then the above query will yield 'Not found'
 		});
 
-    log(loglevels.VERB, "Force-"+(force_disable?"disabling":"enabling")+" HSTS for: "+fullh+" "+details.responseHeaders[details.responseHeaders.length-1].value);
+    logverb("Force-"+(force_disable?"disabling":"enabling")+" HSTS for: "+fullh+" "+details.responseHeaders[details.responseHeaders.length-1].value);
 		return {responseHeaders: details.responseHeaders};
 	},
 	{
