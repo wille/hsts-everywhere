3054a2c1b2a1071b6e329d885e21470d202cd104
wille <w.sandstrom@gmail.com>
2019-05-09 21:18:04 +0200

    upgrade, eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /// <reference path="../node_modules/web-ext-types/global/index.d.ts" />
 
-const matcher = require('matcher');
-const ignoreRules = require('./rules');
-
+import matcher from 'matcher';
+import ignoreRules from './rules.json';
+ 
 // Default max-age 6 months in seconds
 const max_age = "15570000";
 
@@ -10,30 +10,32 @@ if (chrome) {
   browser = chrome;
 }
 
-browser.webRequest.onHeadersReceived.addListener(
-	function(details) {
-    const url = new URL(details.url);
+function onHeadersReceived(details) {
+	const url = new URL(details.url);
 
-    if (matcher([url.hostname], ignoreRules).length > 0) {
-      console.log("Blocked HSTS enforcement on:", details.url);
+	if (matcher([url.hostname], ignoreRules).length > 0) {
+		console.log("Blocked HSTS enforcement on:", details.url);
+		return;
+	}
+
+	for (const header of details.responseHeaders) {
+		if (header.name.toLowerCase() === "strict-transport-security") {
+			console.log("Skipping because of existing header:", details.url);
 			return;
-    }
-
-		for (const header of details.responseHeaders) {
-			if (header.name.toLowerCase() === "strict-transport-security") {
-        console.log("Skipping because of existing header:", details.url);
-        return;
-      }
 		}
+	}
 
-		details.responseHeaders.push({
-			name: "Strict-Transport-Security",
-			value: `max-age="${max_age}";`
-		});
-	},
+	details.responseHeaders.push({
+		name: "Strict-Transport-Security",
+		value: `max-age="${max_age}";`
+	});
+}
+
+browser.webRequest.onHeadersReceived.addListener(
+	onHeadersReceived,
 	{
 		urls: ["https://*/*"],
 		types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
 	},
-	["blocking", "responseHeaders" ]
+	["blocking", "responseHeaders"]
 );
